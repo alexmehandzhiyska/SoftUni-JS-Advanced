baebc2b7d37d2337aa8bcdf20dae57ec06583fba
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2021-01-31 15:19:55 +0200

    Update 01. Fruit.js

--- 01. Exercise: Syntax, Functions and Statements/01. Fruit.js
+++ 01. Exercise: Syntax, Functions and Statements/01. Fruit.js
@@ -1,3 +1,3 @@
 function solve(arg1, arg2, arg3) {
-    console.log(`I need ${(arg2 / 1000 * arg3).toFixed(2)} to buy ${(arg2 / 1000).toFixed(2)} kilograms ${arg1}.`);
+    console.log(`I need $${(arg2 / 1000 * arg3).toFixed(2)} to buy ${(arg2 / 1000).toFixed(2)} kilograms ${arg1}.`);
 }
